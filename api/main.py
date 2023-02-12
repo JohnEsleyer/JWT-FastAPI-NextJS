63702589bece640fef32631d9f5eab02f3557716
john <ralphesleyer@gmail.com>
2023-02-12 11:16:43 +0800

    connecting api to SQLite

--- api/main.py
+++ api/main.py
@@ -1,30 +1,12 @@
-
-from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware 
+from fastapi import FastAPI, HTTPException 
+from fastapi.responses import JSONResponse 
+from pydantic import BaseModel 
+import sqlite3
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True, 
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# Connect to SQLite database
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor() 
 
-class LoginData(BaseModel):
-    username: str
-    password: str
 
-@app.post("/login")
-async def login(data: LoginData):
-
-    print(data.username)
-    print(data.password)
-    # Verify the username and password
-    if data.username == "admin" and data.password == "secret":
-        # Generate the JWT
-        return {"access_token": "jwt_token"}
-    else:
-        return {"message": "Incorrect username or password"}
